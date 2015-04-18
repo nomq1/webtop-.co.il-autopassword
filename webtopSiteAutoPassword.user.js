24623f1ce1733126e98a3d3e974d5f5204ab470a
Amit <amitp01@walla.com>
2015-04-18 18:02:49 +0300

    Update webtopSiteAutoPassword.user.js

--- webtopSiteAutoPassword.user.js
+++ webtopSiteAutoPassword.user.js
@@ -1,7 +1,7 @@
 // Allow Password Remembering on webtop
 // version 0.5
-// 2005-04-06
-// Copyright (c) 2005, Amit Palti 
+// 2015-04-18
+// Copyright (c) 2015, Amit Palti
 // Released under the MIT license
 // --------------------------------------------------------------------
 //
